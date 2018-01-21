fc19035f440c1cca67b582f3d36064ffc9491527
Perceval FARAMAZ <perfaram@windowslive.com>
2018-01-21 20:36:39 +0100

    Proper article splitting on index.php/homepage
    Signed-off-by: Perceval FARAMAZ <perfaram@windowslive.com>

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php get_header(); ?>
 
-<article>
-
-		<?php
-		if ( have_posts() ) :
+<?php
+	if ( have_posts() ) {
 			if ( is_home() && ! is_front_page() ) : ?>
 				<header>
 					<h1><?php single_post_title(); ?></h1>
@@ -12,13 +10,14 @@
 			<?php
 			endif;
 			while ( have_posts() ) : the_post();
+		?><article><?php
 				get_template_part( 'content', get_post_format() );
+		?></article> <?php
 			endwhile;
 			the_posts_navigation();
-		else :
+	} else {
 			get_template_part( 'content', 'none' );
-		endif; ?>
-
-	</article><!-- #primary -->
+	}
+?>
 
 <?php get_footer(); ?>
