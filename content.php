9f01663a2e6df3f3287512642bc7ee8909369419
Perceval FARAMAZ <perfaram@windowslive.com>
2018-01-21 20:31:16 +0100

    Article styling (date/read more tag/link in h1)
    Signed-off-by: Perceval FARAMAZ <perfaram@windowslive.com>

--- content.php
+++ content.php
@@ -1,15 +1,16 @@
 <h2>
-  <?php the_title(); ?>
+	<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
 </h2>
-<p>
-  <?php
-          if ( has_post_thumbnail() )
-          the_post_thumbnail( 'large' );
-        ?>
-</p>
+<?php
+	if ( has_post_thumbnail() ) {
+		echo "<p>";
+		the_post_thumbnail( 'large' );
+		echo "</p>";
+	}
+?>
 
-<?php the_content(); ?>
+<?php the_content('<br><em>voir plus…</em>'); ?>
 
-<small>Published on
-<?php the_time('F j, Y'); ?>
+<small>PUBLIÉ LE
+	<?php echo strtoupper(get_the_time("j F")); ?> DE L'AN DE GRÂCE <?php the_time("Y"); ?>.
 </small>
